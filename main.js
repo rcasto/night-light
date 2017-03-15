3c938d4ea77940d132885ee0b1b88c92ff7c2c1f
Richie Casto <Richie Casto>
2017-03-14 22:43:14 -0700

    done testing, real configuration

--- main.js
+++ main.js
@@ -8,14 +8,10 @@ var nightLightPin = 7;
 function init() {
     rpio.open(lightSensorPin, rpio.INPUT);
     rpio.open(nightLightPin, rpio.OUTPUT);
-    // Start the night light circuit at 9pm
-    // cron.schedule('0 21 * * *', () => start());
-    // Stop the night light circuit at 7am
-    // cron.schedule('0 7 * * *', () => stop());
-    // start at 10:37pm for testing
-    cron.schedule('40 22 * * *', () => start());
-    // stop at 10:39pm for testing
-    cron.schedule('38 22 * * *', () => stop());
+    // Start the night light circuit at 9:30pm
+    cron.schedule('30 21 * * *', () => start());
+    // Stop the night light circuit at 6:30am
+    cron.schedule('30 6 * * *', () => stop());
     // Start the night light circuit immediately
     // the tasks will put it back into the regular rhythm
     start();
